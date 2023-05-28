36556244e517ea225a1fcdb28c3c432d7d42d1e9
raniabns <bensalemrania0@gmail.com>
2023-05-28 22:33:46 +0100

    Initial commit

---
+++ CongeSpring/src/main/java/com/example/congespring/CongeSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.congespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CongeSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CongeSpringApplication.class, args);
+    }
+
+}
---
+++ CongeSpring/src/test/java/com/example/congespring/CongeSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.congespring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CongeSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
