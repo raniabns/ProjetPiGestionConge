37c1167bea7409dc8d3266ac8b425d4974aeb515
hatem-jmai <57534253+hatem-jmai@users.noreply.github.com>
2023-05-31 00:31:31 +0100

    create entities

--- CongeSpring/src/main/java/com/example/congespring/Entity/Employe.java
+++ CongeSpring/src/main/java/com/example/congespring/Entity/Employe.java
@@ -1,4 +1,72 @@
 package com.example.congespring.Entity;
 
 public class Employe {
+
+    private int idEmploye;
+    private String nom;
+    private String prenom;
+    private String mail;
+    private String motPasse;
+
+    public Employee() {
+    }
+
+    public Employee(int idEmploye, String nom, String prenom, String mail, String motPasse) {
+        this.idEmploye = idEmploye;
+        this.nom = nom;
+        this.prenom = prenom;
+        this.mail = mail;
+        this.motPasse = motPasse;
+    }
+
+    public int getIdEmploye() {
+        return idEmploye;
+    }
+
+    public void setIdEmploye(int idEmploye) {
+        this.idEmploye = idEmploye;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public String getMotPasse() {
+        return motPasse;
+    }
+
+    public void setMotPasse(String motPasse) {
+        this.motPasse = motPasse;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "idEmploye=" + idEmploye +
+                ", nom='" + nom + '\'' +
+                ", prenom='" + prenom + '\'' +
+                ", mail='" + mail + '\'' +
+                ", motPasse='" + motPasse + '\'' +
+                '}';
+    }
 }
--- CongeSpring/src/main/java/com/example/congespring/Entity/Equipe.java
+++ CongeSpring/src/main/java/com/example/congespring/Entity/Equipe.java
@@ -1,4 +1,36 @@
 package com.example.congespring.Entity;
 
 public class Equipe {
+
+    private int idEquipe;
+    private String nom;
+
+    public Equipe() {
+    }
+
+    public Equipe(int idEquipe, String nom) {
+        this.idEquipe = idEquipe;
+        this.nom = nom;
+    }
+
+    public int getIdEquipe() {
+        return idEquipe;
+    }
+
+    public void setIdEquipe(int idEquipe) {
+        this.idEquipe = idEquipe;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    @Override
+    public String toString() {
+        return "Equipe{" + "idEquipe=" + idEquipe + ", nom='" + nom + '\'' + '}';
+    }
 }
