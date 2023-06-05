c214a4a3b97f23aa6dc6b2dcb1a3ce57b4197702
Mohamed Omar Skhiri <mohamedomar.skhiri@esprit.tn>
2023-06-05 21:50:13 +0100

    First commit

---
+++ CongeSpring/src/main/java/com/example/congespring/Controller/ReclamationController.java
@@ -0,0 +1,4 @@
+package com.example.congespring.Controller;
+
+public class ReclamationController {
+}
--- CongeSpring/src/main/java/com/example/congespring/Entity/Reclamation.java
+++ CongeSpring/src/main/java/com/example/congespring/Entity/Reclamation.java
@@ -2,38 +2,4 @@ package com.example.congespring.Entity;
 
 public class Reclamation {
 
-    private int idReclamation;
-    private String description;
-
-    public Reclamation() {
-    }
-
-    public Reclamation(int idReclamation, String description) {
-        this.idReclamation = idReclamation;
-        this.description = description;
-    }
-
-    public int getIdReclamation() {
-        return idReclamation;
-    }
-
-    public void setIdReclamation(int idReclamation) {
-        this.idReclamation = idReclamation;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    @Override
-    public String toString() {
-        return "Reclamation{" +
-                "idReclamation=" + idReclamation +
-                ", description='" + description + '\'' +
-                '}';
-    }
 }
